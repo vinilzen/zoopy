165c46ca0474cd31e34a6b68d70087c8db8ea3ec
vinilzen <marchukilya@gmail.com>
2016-02-08 12:09:18 +0100

    nav bar

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-/*ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [55.751574, 37.573856],
-            zoom: 9
-        });
-    myMap.behaviors.disable("scrollZoom");
-});*/
 
 $(function(){
-    var ww = $(window).width(),
+    var myMap,
+        animate_time = 300,
+        ww = $(window).width(),
         $c = $('body').find('.container'),
         $mrc = $('body').find('.main-right-column'),
         fixed_width = 1599,
         gap = 28;
+
+    ymaps.ready(function () {
+        myMap = new ymaps.Map('map', {
+            center: [55.751574, 37.573856],
+            zoom: 9
+        });
+        myMap.behaviors.disable("scrollZoom");
+        $( window ).scroll(function() {
+            myMap.container.fitToViewport();
+        })
+    });
 
     setMapWidth(ww);
     setHrWidth(ww);
@@ -51,7 +57,23 @@ $(function(){
         }
     }
 
-    $('.navbar-main').affix({
-        offset: 30
+    $('.navbar-filter').affix({
+        offset: 60
+    }).on('affix.bs.affix', function(){
+        $('.main-content').css({'padding-top': 122});
+    }).on('affix-top.bs.affix', function(){
+        $('.main-content').css({'padding-top': 0});
     });
+
+    $( window ).scroll(function() {
+
+        /* Resize MAP */
+        var st = $(window).scrollTop();// console.log(st);
+        if ( st < 63 ) {
+            $('#map').css({top: 182-st});
+        } else {
+            $('#map').css({top: 123});
+        }
+    });
+
 })
